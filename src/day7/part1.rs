0cf843a03fcfedf6c2e4bf5dfa34a6932719b270
Robert Jakes <jakesrob@protonmail.com>
2023-01-18 00:58:26 +0100

    add day7

---
+++ src/day7/part1.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let input = std::fs::read_to_string("src/day6/input/input.txt").unwrap();
+    //TODO
+    //parse the input
+    //composite pattern struct
+
+    //iterrate tree and sum all dirs which are above threshold
+}
